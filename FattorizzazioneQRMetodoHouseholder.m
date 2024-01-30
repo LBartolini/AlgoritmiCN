f7398b3a9bd25c6b7307121f426f25c9a4d394ac
LBartolini <l.bartolini02@gmail.com>
2024-01-30 11:52:44 +0100

    continua a non funzionare

--- FattorizzazioneQRMetodoHouseholder.m
+++ FattorizzazioneQRMetodoHouseholder.m
@@ -11,9 +11,10 @@ for i=1:n
     end
 
     v1 = A(i,i)-alfa;
+    A(i,i)=alfa;
     A(i+1:m, i) = A(i+1:m, i) / v1;
     beta = -v1/alfa;
-    A(i+1:m, i+1:n) = A(i+1:m, i+1:n) - (beta*[1; A(i+1:m, i)])*([1; A(i+1:m, i)]'.*A(i+1:m, i+1:n));
+    A(i:m, i+1:n) = A(i:m, i+1:n) - (beta*[1; A(i+1:m, i)])*([1 A(i+1:m, i)']*A(i:m, i+1:n));
 end
 
 disp(A);
